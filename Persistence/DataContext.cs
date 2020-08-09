cf65b286b79d4316f730964fbd4d08b9f6ef3b5b
saberdev <saber.softs@gmail.com>
2020-08-10 00:15:59 +0430

    Initial commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Value>()
+                .HasData(
+                    new Value { Id = 1, Text = "Value 101" },
+                    new Value { Id = 2, Text = "Value 102" },
+                    new Value { Id = 3, Text = "Value 103" },
+                    new Value { Id = 4, Text = "Value 104" },
+                    new Value { Id = 5, Text = "Value 105" }
+                );
+        }
+    }
+}
---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger, DataContext dataContext)
+        {
+            _logger = logger;
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var result = await _dataContext.Values.ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Value>>> Get(int id)
+        {
+            var result = await _dataContext.Values.FindAsync(id);
+            return Ok(result);
+        }
+    }
+}
